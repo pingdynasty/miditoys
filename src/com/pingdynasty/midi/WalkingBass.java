c1394008cca212ea597c56c418ae3737eee8c2d9
mars <mars>
2007-03-19 15:50:17 +0000

    updates

--- src/com/pingdynasty/midi/WalkingBass.java
+++ src/com/pingdynasty/midi/WalkingBass.java
@@ -90,17 +90,50 @@ class Player  {
         throws InvalidMidiDataException{
         this.channel = channel;
     }
+
+    public void allNotesOff()
+        throws InvalidMidiDataException{
+        ShortMessage msg = new ShortMessage();
+        msg.setMessage(ShortMessage.CONTROL_CHANGE,  channel, 123, 0);
+        receiver.send(msg, -1);
+    }
 }
 
 public class WalkingBass extends JFrame {
     final Player player;
-    private boolean noteOn[] = new boolean[1024]; // keep track of notes that are on
-    private int[] steps = new int[]{-2, -1, -1, -1, 0, 0, 1, 1, 1, 2};
-    private double skew = 0.10;
+    private boolean noteOn[] = new boolean[512]; // keep track of notes that are on
+    private int[] steps = new int[]{-2, -2, -1, -1, 1, 1, 1, 1, 2, 2};
+    private double skew = 0.20;
     private boolean normal = false; // use normal or uniform distribution
     private int duration = 100; // note duration
     private int period = 500; // time between notes
     private static int channel = 0;
+    private int[][] scales = new int[][]{
+        // C minor blues scale: C Eb F F# G Bb C
+        {0, 3, 5, 6, 7, 10},
+        // C major blues scale: C D D# E G A C
+        {0, 2, 3, 4, 7, 9},
+
+// the seven modes of the diatonic major scale and added-note scales.
+// Ionian mode 	C D E F G A B C 	(associated with C Major 7 chord)
+               {0,2,4,5,7,9,11},
+
+// Dorian mode 	C D Eb F G A Bb C 	(associated with C-7 chord)
+               {0,2,3, 5,7,9,12},
+
+// Phrygian mode C Db Eb F G Ab Bb C 	(associated with C Phrygian chord)
+                {0,1, 3, 5,7,10,12},
+// Lydian mode 	C D E F# G A B C 	(associated with C Maj7 #4 chord)
+               {0,2,4,6, 7,9,11},
+// Mixolydian mode C D E F G A Bb C 	(associated with C7 chord)
+                  {0,2,4,5,7,9,12},
+
+// Aeolian mode D Eb F G Ab Bb C 	(associated with C-7 b6 chord)
+               {2,3, 5,7,8, 12},
+// Locrian mode	C Db Eb F Gb Ab Bb C 	(associated with C-7b5 chord)
+               {0,1, 3, 5,6, 8, 12}
+    };
+    int scaleindex = 0;
 
     public static void main(String[  ] args) 
         throws MidiUnavailableException, Exception {
@@ -135,6 +168,9 @@ public class WalkingBass extends JFrame {
         addKeyListener(new KeyAdapter( ) {
                 public void keyPressed(KeyEvent e) {
                     int key = e.getKeyCode();
+//                     System.out.println("key "+KeyEvent.getKeyText(key)+" ("+key+")");
+                   if(key < 0x30 || key > 0x5a)
+                        return;
                     try{
                         if(key == KeyEvent.VK_LEFT){
                             player.setChannel(--channel);
@@ -142,8 +178,12 @@ public class WalkingBass extends JFrame {
                         }else if(key == KeyEvent.VK_RIGHT){
                             player.setChannel(++channel);
                             System.out.println("channel "+channel);
+                        }else if(key == KeyEvent.VK_ESCAPE){
+                            player.allNotesOff();
+                            player.bend(0);
+                            player.modulate(0);
                         }else{
-                            int note = getNote(key);
+                            int note = getNote(key - 36);
                             if(note > 0 && !noteOn[note]){
                                 try{
                                     noteOn[note] = true;
@@ -158,20 +198,23 @@ public class WalkingBass extends JFrame {
                     }
                 }
                 public void keyReleased(KeyEvent e) {
+                    int key = e.getKeyCode();
+                    if(key < 0x30 || key > 0x5a)
+                        return;
                     KeyEvent nextPress = (KeyEvent)Toolkit.getDefaultToolkit().
                         getSystemEventQueue().
                         peekEvent(KeyEvent.KEY_PRESSED);
-                    if ((nextPress == null) ||
-                        (nextPress.getWhen() != e.getWhen()) ||
-                        (nextPress.getKeyCode() != e.getKeyCode())) {
-                        int note = getNote(e.getKeyCode());
-                        if(note > 0 && noteOn[note]){
-                            try{
-                                noteOn[note] = false;
-                                player.noteoff(note);
-                            }catch(Exception exc){
-                                exc.printStackTrace();
-                            }
+                    if((nextPress != null) &&
+                       (nextPress.getWhen() == e.getWhen()) &&
+                       (nextPress.getKeyCode() == key))
+                        return;
+                    int note = getNote(key - 36);
+                    if(note > 0 && noteOn[note]){
+                        try{
+                            noteOn[note] = false;
+                            player.noteoff(note);
+                        }catch(Exception exc){
+                            exc.printStackTrace();
                         }
                     }
                 }
@@ -179,7 +222,7 @@ public class WalkingBass extends JFrame {
         addMouseMotionListener(new MouseMotionAdapter( ) {
                 public void mouseMoved(MouseEvent event) {
                     int velocity = event.getX()/2;
-                    System.out.println("velocity "+velocity);
+//                     System.out.println("velocity "+velocity);
                     player.setVelocity(velocity);
                     duration = event.getY() * 2;
 //                     int bend = 128 - event.getY()/2;
@@ -224,8 +267,75 @@ public class WalkingBass extends JFrame {
            }
         }
         menubar.add(menu);
-        setJMenuBar(menubar);
 
+        menu = new JMenu("Scales");
+        JMenuItem item = new JMenuItem("C minor blues scale");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 0;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("C major blues scale");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 1;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("Ionian mode");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 2;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("Dorian mode");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 3;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("Phrygian mode");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 4;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("Lydian mode");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 5;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("Mixolydian mode");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 6;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("Aeolian mode");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 7;
+                }
+            });
+        menu.add(item);
+        item = new JMenuItem("Locrian mode");
+        item.addActionListener(new ActionListener(  ) {
+                public void actionPerformed(ActionEvent event) {
+                    scaleindex = 8;
+                }
+            });
+        menu.add(item);
+
+        menubar.add(menu);
+
+        setJMenuBar(menubar);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(255, 255);
         setVisible(true);
@@ -237,7 +347,8 @@ public class WalkingBass extends JFrame {
         int direction = 1; // 1 or -1 depending on previous direction - up or down.
         double rand;
         int bucket;
-        int note = 61;
+        int key = (60 / 12) * scales[scaleindex].length; // Middle C equivalent position
+        int note = 0;
         for(;;){
             if(normal)
                 rand = random.nextGaussian(); // Gaussian (normal) distribution
@@ -246,12 +357,11 @@ public class WalkingBass extends JFrame {
             rand *= steps.length - 1;
             rand += (skew * direction);
             bucket = (int)Math.round(rand);
-//             bucket = (int)Math.floor(rand);
             if(bucket >= steps.length)
                 bucket = steps.length - 1;
-            System.out.println("bucket "+bucket+": "+steps[bucket]+" ("+rand+")");
-            note += steps[bucket];
+            key += steps[bucket];
             direction = bucket > (steps.length / 2) ? 1 : -1;
+            note = getNote(key);
             player.noteon(note);
             Thread.currentThread().sleep(duration);
             player.noteoff(note);
@@ -261,9 +371,12 @@ public class WalkingBass extends JFrame {
 
     /** map a keypress code to a MIDI note */
     public int getNote(int key){
-        System.out.println("key "+KeyEvent.getKeyText(key)+" ("+key+")");
-        if(key >= 0x30 && key <= 0x5a)
-            return key;
-        return 0;
+        int note = scales[scaleindex][key % scales[scaleindex].length];
+        System.out.println("key "+key+" ("+key % scales[scaleindex].length+"): "+note);
+        System.out.println("key / scales[scaleindex].length = "+key / scales[scaleindex].length);
+        note += (key / scales[scaleindex].length) * 12;
+        System.out.println("note "+note);
+        // 12 is the length of an octave in midi notes
+        return note;
     }
 }
--- walkingbass.sh
+++ walkingbass.sh
@@ -1,2 +1,3 @@
-javac -d build/classes src/com/pingdynasty/midi/WalkingBass.java && \
+CP=$JAVA_HOME/../Classes/classes.jar
+jikes -classpath $CP -d build/classes src/com/pingdynasty/midi/WalkingBass.java && \
 java -classpath lib/plumstoneserv.jar:build/classes com.pingdynasty.midi.WalkingBass
