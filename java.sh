ee0651ca24b97c8db279511ae4805c4a777c52b8
mars <mars>
2007-04-23 07:24:12 +0000

    include all jar files

--- java.sh
+++ java.sh
@@ -1,3 +1,6 @@
-CP=$JAVA_HOME/../Classes/classes.jar:build/classes:src/properties:lib/plumstoneserv.jar:lib/jinput.jar:lib/jutils.jar:lib/jvaptools.jar:lib/jVSTwRapper_bin.jar:lib/jVSTsYstem_bin.jar
+CP=$JAVA_HOME/../Classes/classes.jar:build/classes:src/properties
+for nm in lib/*.jar
+do CP=$CP:$nm
+done
 javac -classpath $CP -d build/classes `find src/com -name '*.java'` && \
 java -Djava.library.path=lib/native/macosx -classpath $CP $@
