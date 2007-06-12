dedcf01175e656fb584b9b89f3afd33c4f076e45
mars <mars>
2007-06-12 08:02:22 +0000

    applet for Beat Slicer

---
+++ src/com/pingdynasty/midi/bcontrol/BCRBeatSlicerApplet.java
@@ -0,0 +1,34 @@
+package com.pingdynasty.midi.bcontrol;
+
+import java.io.File;
+import java.net.URL;
+import javax.swing.*;
+
+public class BCRBeatSlicerApplet extends JApplet  {
+
+    private BCRBeatSlicer beats;
+
+    public void init() {
+        try{
+            beats = new BCRBeatSlicer();
+            beats.initialiseMidiDevices();
+            String sample = getParameter("sample");
+            if(sample != null && !sample.equals(""))
+                beats.loadSample(new URL(getCodeBase(), sample));
+        }catch(Exception exc){exc.printStackTrace();}
+        setContentPane(beats);
+        setJMenuBar(beats.getMenuBar());
+        setSize(625, 550);
+        setVisible(true);
+    }
+
+    public void start() {
+    }
+
+    public void stop() {
+    }
+
+    public void destroy() {
+        beats.destroy();
+    }
+}
