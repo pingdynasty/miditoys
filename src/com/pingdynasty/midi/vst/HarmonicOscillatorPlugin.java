e45167d500bbe8349e387a1504d1fc258694c743
mars <mars>
2007-07-17 10:36:14 +0000

    removed processing mode settings

--- src/com/pingdynasty/midi/vst/HarmonicOscillatorPlugin.java
+++ src/com/pingdynasty/midi/vst/HarmonicOscillatorPlugin.java
@@ -14,27 +14,15 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
 //     private double scaleconstant = 2.0d * 32768.0d / 127.0d;
 //     private double scaleconstant = 2.0d / 127.0d;
     private double scaleconstant = 2.0d;
-    private boolean running = false;
     private int framepos;
-    private int mode = MUL_MODE;
     private static final int CONTROLS = 16;
+    private static final int SAMPLE_WIDTH = 512;
     private static final String[] PARAMETER_NAMES = new String[]{
-        "mode",
         "wavelength",
         "distance",
         "time step",
         "scale",
         "glauber state"
-    };
-    private static final int MUL_MODE = 0;
-    private static final int ADD_MODE = 1;
-    private static final int SUB_MODE = 2;
-    private static final int DIV_MODE = 3;
-    private static final String[] MODE_NAMES = new String[]{
-        "mul",
-        "add",
-        "sub",
-        "div"
     };
 
 //     public native void setOutputSamplerate(float);
@@ -53,16 +41,7 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
         this.isSynth(false);
         this.setUniqueID(UNIQUE_ID);
         this.suspend();
-
-        osc = new HarmonicOscillator(getBlockSize() * 4, CONTROLS);
-
-//         // initialise sound
-//         MidiDevice device = getMidiDevice();
-//         try{
-//             osc.initSound(device);
-//         }catch(Exception exc){
-//             exc.printStackTrace();
-//         }
+        osc = new HarmonicOscillator(SAMPLE_WIDTH, CONTROLS);
     }
 
     /* called from HarmonicOscillatorGUI.init() */
@@ -71,29 +50,21 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
 //         setParameter(4, 0.5f); // set scale factor
     }
 
-    public void open() {
-        super.open();
-        System.out.println("harms effect plugin opened");
-    }
+//     public void open() {
+//         super.open();
+//         System.out.println("harms effect plugin opened");
+//     }
 
-    public void close() {
-        super.close();
-        //         osc.close();
-        System.out.println("harms effect plugin closed");
-    }
+//     public void close() {
+//         super.close();
+//         //         osc.close();
+//         System.out.println("harms effect plugin closed");
+//     }
 
-    public void resume() { 
-        System.out.println("resume");
-        running = true;
-    }
-    public void suspend() { 
-        System.out.println("suspend");
-        running = false;
-    }
-    public int startProcess() { System.out.println("start process"); return 0;}
-    public int stopProcess() { System.out.println("stop process"); return 0;}
+//     public int startProcess() { System.out.println("start process"); return 0;}
+//     public int stopProcess() { System.out.println("stop process"); return 0;}
     public boolean setBypass(boolean value) {
-        System.out.println("bypass "+value);
+        log("harms effect bypass "+value);
         return false; 
     }
 
@@ -126,25 +97,25 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
         return CANDO_NO;
     }
 
-    public void setBlockSize(int size) {
-        System.out.println("set block size "+size);
-        osc = new HarmonicOscillator(size * 4, CONTROLS);
-    }
+//     public void setBlockSize(int size) {
+//         System.out.println("set block size "+size);
+//         osc = new HarmonicOscillator(size * 4, CONTROLS);
+//     }
 
-    public void setSampleRate(float rate) {
-        System.out.println("set sample rate "+rate);
-    }
+//     public void setSampleRate(float rate) {
+//         System.out.println("set sample rate "+rate);
+//     }
 
-    public boolean setProcessPrecision(int precision){
-        System.out.println("set precision "+precision);
-//         if(precision == 16)
-//             scaleconstant = 2.0d * 32768.0d / 127.0d;
-//         else if(precision == 32)
-//             scaleconstant = 2.0d * 2147483648.0d / 127.0d;
-//         else
-//             return false;
-        return true;
-    }
+//     public boolean setProcessPrecision(int precision){
+//         System.out.println("set precision "+precision);
+// //         if(precision == 16)
+// //             scaleconstant = 2.0d * 32768.0d / 127.0d;
+// //         else if(precision == 32)
+// //             scaleconstant = 2.0d * 2147483648.0d / 127.0d;
+// //         else
+// //             return false;
+//         return true;
+//     }
 
     public int getNumParams() { 
         return PARAMETER_NAMES.length + CONTROLS; 
@@ -158,14 +129,6 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
 //         return "display "+index;
 //     }
 
-    public String getParameterDisplay(int index) {
-        switch(index){
-        case 0:
-            return MODE_NAMES[mode];
-        }
-        return "";
-    }
-
     public String getParameterName(int index) {
         if(index < PARAMETER_NAMES.length)
             return PARAMETER_NAMES[index];
@@ -174,34 +137,24 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
 
     public void setParameter(int index, float value){
         int val = (int)(value * 127.0f);
-        System.out.println("set parameter "+index+": "+value+"("+val+")");
+//         System.out.println("set parameter "+index+": "+value+"("+val+")");
         switch(index){
         case 0:
-            if(value < 0.25)
-                mode = MUL_MODE;
-            else if(value < 0.50)
-                mode = ADD_MODE;
-            else if(value < 0.75)
-                mode = SUB_MODE;
-            else
-                mode = DIV_MODE;
-            break;
-        case 1:
             osc.setWavelength(val);
             break;
-        case 2:
+        case 1:
             osc.setDistance(val);
             break;
-        case 3:
+        case 2:
             osc.setTimeStep(val);
             break;
-        case 4:
+        case 3:
 //             scale =  val * scaleconstant;
             scale =  value * scaleconstant;
             if(view != null)
                 view.setScaleFactor(val);
             break;
-        case 5:
+        case 4:
             osc.setEnergy(val);
             for(int i=0; i<CONTROLS; ++i)
                 setParameterAutomated(i+PARAMETER_NAMES.length, osc.getControl(i));
@@ -217,22 +170,19 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
         int val = 0;
         switch(index){
         case 0:
-            val = 127 / (MODE_NAMES.length - mode);
-            break;
-        case 1:
             val = osc.getWavelength();
             break;
-        case 2:
+        case 1:
             val = osc.getDistance();
             break;
-        case 3:
+        case 2:
             val = osc.getTimeStep();
             break;
-        case 4:
+        case 3:
 //             return (float)(scale / scaleconstant);
             val = (int)(scale / scaleconstant * 127.0d);
             break;
-        case 5:
+        case 4:
             val = osc.getEnergy();
             break;
         default:
@@ -264,75 +214,25 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
 
     /* Deprecated as of VST 2.4 */
     public void process(float[][] inputs, float[][] outputs, int frames) {
-        System.out.println("process");
         processReplacing(inputs, outputs, frames);
     }
 
     // Audio data processed by VST Plug-Ins is 32 bit (single precision) and optionally 64 bit (double precision) floating-point data. The default used range is from -1.0 to +1.0 inclusive [-1.0, +1.0] (where 1.0 corresponds to 0dB, 0.5 to -6dB and 0.0 to -oodB). Note that an effect could generate values above this range.
     public void processReplacing(float[][] inputs, float[][] outputs, int frames) {
-        if(running){
-            if(framepos == 0)
-                osc.calculate();
-            double[] values = osc.getData();
-            switch(mode){
-            case MUL_MODE :
-                for(int i=0; i<frames && framepos<values.length; ++i)
-                    outputs[0][i] = inputs[0][i] * (float)(values[framepos++] * scale);
-                break;
-            case ADD_MODE :
-                for(int i=0; i<frames && framepos<values.length; ++i)
-                    outputs[0][i] = inputs[0][i] + (float)(values[framepos++] * scale);
-                break;
-            case SUB_MODE :
-                for(int i=0; i<frames && framepos<values.length; ++i)
-                    outputs[0][i] = inputs[0][i] - (float)(values[framepos++] * scale);
-                break;
-            case DIV_MODE :
-                for(int i=0; i<frames && framepos<values.length; ++i)
-                    outputs[0][i] = inputs[0][i] + (float)(values[framepos++] * scale) / 2.0f;
-//                 for(int i=0; i<frames && framepos<values.length; ++i){
-//                     float value = (float)(values[framepos++] * scale);
-//                     if(value == 0)
-//                         outputs[0][i] = inputs[0][i];
-//                     else
-//                         outputs[0][i] = inputs[0][i] / value;
-//                 }
-                break;
-            }
-            if(framepos == values.length){
-                framepos = 0;
-                osc.increment();
-                if(view != null)
-                    view.setAndScaleData(osc.getData());
-            }
+        if(framepos == 0)
+            osc.calculate();
+        double[] values = osc.getData();
+        for(int i=0; i<frames && framepos<values.length; ++i)
+            outputs[0][i] = inputs[0][i] * (float)(values[framepos++] * scale);
+        if(framepos == values.length){
+            framepos = 0;
+            osc.increment();
+            if(view != null)
+                view.setAndScaleData(osc.getData());
         }
     }
 
-//     public void processReplacing(float[][] inputs, float[][] outputs, int frames) {
-//         if(running){
-// //             System.out.println("process replacing");
-//             double[] values = osc.calculate();
-//             if(frames != values.length)
-//                 System.out.println("frames "+frames+"/"+values.length+"/"+outputs[0].length);
-//             if(outputs.length != 1 || outputs[0].length != frames)
-//                 System.out.println("frames "+frames+"/"+outputs.length+"/"+outputs[0].length);
-//             for(int i=0; i<frames; ++i)
-//             outputs[0][i] = (float)(values[i] * scale);
-// //         int pos = 0;
-// //         while(frames > 0) {
-// //             int length = frames > values.length ? values.length : frames;
-// //             for(int i=0; i<length; ++i)
-// //                 outputs[0][pos++] = (float)(values[i] * scale);
-// //             frames -= length;
-// //         }
-//             osc.increment();
-//             if(view != null)
-//                 view.setAndScaleData(osc.getData());
-//         }
-//     }
-
 //     public void processDoubleReplacing(double[][] inputs, double[][] outputs, int frames) {
-//         if(running){
 //         System.out.println("process double");
 //         double[] values = osc.calculate();
 //         int pos = 0;
@@ -345,7 +245,6 @@ public class HarmonicOscillatorPlugin extends VstPluginImpl {
 //         osc.increment();
 //         if(view != null)
 //             view.setAndScaleData(osc.getData());
-//         }
 //     }
 
 }
