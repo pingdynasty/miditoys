b2e4cd1bd9cb51c3d6dc5b698f28a17850b8c655
mars <mars>
2008-04-23 03:08:31 +0000

    updated scripts

--- java.sh
+++ java.sh
@@ -2,5 +2,5 @@ CP=$JAVA_HOME/../Classes/classes.jar:build/classes:src/properties
 for nm in lib/*.jar
 do CP=$CP:$nm
 done
-javac -classpath $CP -d build/classes `find src/com -name '*.java'` && \
+javac -classpath $CP -d build/classes `find src/com/pingdynasty/midi -name '*.java'` && \
 java -Djava.library.path=lib/native/macosx -classpath $CP $@
--- pong.sh
+++ pong.sh
@@ -1,3 +1,6 @@
-CP=$JAVA_HOME/../Classes/classes.jar:build/classes:src/properties:lib/plumstoneserv.jar:lib/jinput.jar
-javac -classpath $CP -d build/classes src/com/pingdynasty/*/*.java && \
-java -Djava.library.path=lib/native/macosx -classpath $CP com.pingdynasty.pong.Pong
+CP=$JAVA_HOME/../Classes/classes.jar:build/classes:src/properties
+for nm in lib/*.jar
+do CP=$CP:$nm
+done
+javac -classpath $CP -d build/classes src/com/pingdynasty/pong/*.java && \
+java -ea -Djava.library.path=lib/native/macosx -classpath $CP com.pingdynasty.pong.Pong
